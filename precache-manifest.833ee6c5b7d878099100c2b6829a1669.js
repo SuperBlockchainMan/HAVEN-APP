fed32d8b1d5d6bf810fe4215d641013293bd61f4
SuperWebMan <aleksandrshreider@outlook.com>
2021-06-12 04:31:55 -0700

    Add project
    Add project

---
+++ precache-manifest.833ee6c5b7d878099100c2b6829a1669.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744ca7e99c74eff9aa4b3f6f937a76c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e32a905b5ab39b692a19",
+    "url": "/static/js/0.f371d2d2.chunk.js"
+  },
+  {
+    "revision": "01e83370fa97cc19514d",
+    "url": "/static/js/10.23efd898.chunk.js"
+  },
+  {
+    "revision": "37f82b7623b2f7aee577",
+    "url": "/static/js/3.b8c296fa.chunk.js"
+  },
+  {
+    "revision": "638537a98ceb4f250e5680df3d162472",
+    "url": "/static/js/3.b8c296fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe080b7dc637dfd71c7",
+    "url": "/static/js/4.f00ad2bc.chunk.js"
+  },
+  {
+    "revision": "e81d3b654224bb1fb250",
+    "url": "/static/js/5.bdf215aa.chunk.js"
+  },
+  {
+    "revision": "3d493347cbf0ebaf193563102afcd817",
+    "url": "/static/js/5.bdf215aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d9fe09525f6022de5c",
+    "url": "/static/js/6.8b9417b3.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.8b9417b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c4d1a5f7cb8dcbd2232",
+    "url": "/static/js/7.a4cd49f2.chunk.js"
+  },
+  {
+    "revision": "b6b0682bcf5e046bf782",
+    "url": "/static/js/8.1e90dfc9.chunk.js"
+  },
+  {
+    "revision": "716df44872df7dd6e098",
+    "url": "/static/js/9.4227e40c.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/9.4227e40c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08fc0ee7c3e47d29a08",
+    "url": "/static/js/main.30279007.chunk.js"
+  },
+  {
+    "revision": "91e0a1418da1441249dd",
+    "url": "/static/js/runtime-main.c440131d.js"
+  },
+  {
+    "revision": "eb574c81442e70096c6cb1b63ad7e902",
+    "url": "/static/media/bnb_in_pool.eb574c81.png"
+  },
+  {
+    "revision": "0e43b46fc8e2e8ae2f87857e6567953a",
+    "url": "/static/media/current_price.0e43b46f.png"
+  },
+  {
+    "revision": "43454ca9c0a8e0808c3813390cfbe89a",
+    "url": "/static/media/icon.43454ca9.png"
+  },
+  {
+    "revision": "258ca8472bbb8f01e7c5c892d5f3b451",
+    "url": "/static/media/liquidity_pool.258ca847.png"
+  },
+  {
+    "revision": "f467485688da84bee8b2f527f740acc6",
+    "url": "/static/media/logo.f4674856.png"
+  },
+  {
+    "revision": "5113bfdc0d0efc9ab4847e8beb7e56e7",
+    "url": "/static/media/max_transaction_amount.5113bfdc.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "c0cc501e7ed58258221c177fa355a79d",
+    "url": "/static/media/reward_pool.c0cc501e.png"
+  }
+]);
